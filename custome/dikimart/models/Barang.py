f83aa8ad6957f86ba082f94046d7727652f92d16
Diki Hamdani <dikihamdani.id@gmail.com>
2022-08-29 15:30:56 +0700

    UPdate model Barang

--- custome/dikimart/models/Barang.py
+++ custome/dikimart/models/Barang.py
@@ -8,6 +8,10 @@ class Barang(models.Model):
     name = fields.Char(string='Name')
     harga_beli = fields.Integer(string='Harga Modal', required=True)
     harga_jual = fields.Integer(string='Harga Jual', required=True)
+    # Comodel adalah menentukkan model mana yang menjadi one-nya
+    kelompokbarang_id = fields.Many2one(comodel_name='dikimart.kelompokbarang',
+                                        string='Kelompok Barang')
+    
     
     
     
