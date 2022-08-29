2f0d1024eade0551dba9f38006ca20a1d10e3711
Diki Hamdani <dikihamdani.id@gmail.com>
2022-08-29 15:27:43 +0700

    Update model KelompokBarang

--- custome/dikimart/models/KelompokBarang.py
+++ custome/dikimart/models/KelompokBarang.py
@@ -5,8 +5,8 @@ class KelompokBarang(models.Model):
     _name = 'dikimart.kelompokbarang'
     _description = 'New Description'
 
-    name = fields.Char(string='Name')
-    kode_kelompok = fields.Char(string='Kode Kelompok Barang')
+    name = fields.Char(string='Nama Kelompok')
+    kode_kelompok = fields.Char(string='Kode Kelompok')
     kode_rak = fields.Char(string='Kode Rak')
     
     
