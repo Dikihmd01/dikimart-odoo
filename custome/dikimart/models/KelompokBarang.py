988f9dcda530e47c04f7cce9efa85dc6698a279b
Diki Hamdani <dikihamdani.id@gmail.com>
2022-08-29 16:11:48 +0700

    Add one to many fields

--- custome/dikimart/models/KelompokBarang.py
+++ custome/dikimart/models/KelompokBarang.py
@@ -8,5 +8,9 @@ class KelompokBarang(models.Model):
     name = fields.Char(string='Nama Kelompok')
     kode_kelompok = fields.Char(string='Kode Kelompok')
     kode_rak = fields.Char(string='Kode Rak')
+    barang_ids = fields.One2many(comodel_name='dikimart.barang',
+                                inverse_name='kelompokbarang_id',
+                                string='Daftar Barang')
+    
     
     
