5396a5a21335cb23598b78e697d22093158d4113
Diki Hamdani <dikihamdani.id@gmail.com>
2022-08-30 15:29:55 +0700

    Add new models with inheritence

---
+++ custome/dikimart/models/Person.py
@@ -0,0 +1,28 @@
+from odoo import api, fields, models
+
+
+class Person(models.Model):
+    _name = 'dikimart.person'
+    _description = 'New Description'
+
+    name = fields.Char(string='Nama')
+    alamat = fields.Char(string='Alamat')
+    tgl_lahir = fields.Datetime(string='Tanggal Lahir')
+
+
+class Kasir(models.Model):
+    _name = 'dikimart.kasir'
+    _inherit = 'dikimart.person'
+    _description = 'New Description'
+
+    id_kasir = fields.Char(string='ID Kasir')
+
+
+class CleaningService(models.Model):
+    _name = 'dikimart.cleaningservice'
+    _inherit = 'dikimart.person'
+    _description = 'New Description'
+
+    id_cln = fields.Char(string='ID Cleaning Service')
+    
+
