90930ee39b68e2e02f2964198a953d9d9b1359ec
Diki Hamdani <dikihamdani.id@gmail.com>
2022-09-07 10:20:51 +0700

    Add controller

--- custome/dikimart/controllers/controllers.py
+++ custome/dikimart/controllers/controllers.py
@@ -1,21 +1,40 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+from crypt import methods
+import json
 
 
-# class Dikimart(http.Controller):
-#     @http.route('/dikimart/dikimart', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+import json
 
-#     @http.route('/dikimart/dikimart/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('dikimart.listing', {
-#             'root': '/dikimart/dikimart',
-#             'objects': http.request.env['dikimart.dikimart'].search([]),
-#         })
+from odoo import http, models, fields
+from odoo.http import request
 
-#     @http.route('/dikimart/dikimart/objects/<model("dikimart.dikimart"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('dikimart.object', {
-#             'object': obj
-#         })
+
+class Dikimart(http.Controller):
+    @http.route('/dikimart/getbarang', auth='public', method=['GET'])
+    def getBarang(self, **kw):
+        # Mengambil semua barang dari table barang
+        barang = request.env['dikimart.barang'].search([])
+        items = []
+
+        for item in barang:
+            items.append({
+                'nama_barang': item.name,
+                'harga_jual': item.harga_jual,
+                'stok': item.stok
+            })
+        
+        return json.dumps(items)
+
+    @http.route('/dikimart/getsupplier', auth='public', method=['GET'])
+    def getSupplier(self, **kw):
+        supplier = request.env['dikimart.supplier'].search([])
+        items = []
+
+        for item in supplier:
+            items.append({
+                'nama_perusahaan': item.name,
+                'alamat': item.alamat,
+                'no_telepon': item.no_telp,
+                'barang_id': item.barang_id[0].name
+            })
+        
+        return json.dumps(items)
